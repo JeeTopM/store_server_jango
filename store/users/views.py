fbdc1db9561656954a4971d20d45b16731db4ed6
Лев Романович Панас <jieba2005@list.ru>
2025-08-04 19:32:26 +0300

    Личный кабинет v.1

--- store/users/views.py
+++ store/users/views.py
@@ -11,29 +11,37 @@ def login(request):
     if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse("index"))
     else:
         form = UserLoginForm
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+    context = {"form": form}
+    return render(request, "users/login.html", context)
+
 
 def registration(request):
     if request.method == "POST":
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('users:login'))
+            return HttpResponseRedirect(reverse("users:login"))
     else:
         form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/registration.html', context)
+    context = {"form": form}
+    return render(request, "users/registration.html", context)
+
 
 def profile(request):
-    form = UserProfileForm()
-    context = {'title': 'Store - профиль', 'form': form}
-    return render(request, 'users/profile.html', context)
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {"title": "Store - профиль", "form": form}
+    return render(request, "users/profile.html", context)
