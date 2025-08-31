8090c80d7e12697166ec5af8f94a9a46cc92f2c9
Лев Романович Панас <jieba2005@list.ru>
2025-08-31 19:44:53 +0300

    Декораторы доступа

--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -35,7 +36,7 @@ def registration(request):
     context = {"form": form}
     return render(request, "users/registration.html", context)
 
-
+@login_required
 def profile(request):
     if request.method == "POST":
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
