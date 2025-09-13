2ebf629a31b340ab96b1984bd8a2ff129a4f0c53
Лев Романович Панас <jieba2005@list.ru>
2025-09-13 21:42:51 +0300

    EmailVerification

--- store/users/views.py
+++ store/users/views.py
@@ -16,21 +16,6 @@ class UserLoginView(LoginView):
     title = 'Store - авторизация'
 
 
-# def login(request):
-#     if request.method == "POST":
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST["username"]
-#             password = request.POST["password"]
-#             user = auth.authenticate(username=username, password=password)
-#             if user:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse("index"))
-#     else:
-#         form = UserLoginForm
-#     context = {"form": form}
-#     return render(request, "users/login.html", context)
-
 class UserRegistrationView(TitleMixin ,SuccessMessageMixin, CreateView):
     model = User
     template_name = "users/registration.html"
@@ -54,37 +39,3 @@ class UserProfileView(TitleMixin ,UpdateView):
         context = super(UserProfileView, self).get_context_data(**kwargs)
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
-
-# def registration(request):
-#     if request.method == "POST":
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегистрировались!')
-#             return HttpResponseRedirect(reverse("users:login"))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {"form": form}
-#     return render(request, "users/registration.html", context)
-
-# @login_required
-# def profile(request):
-#     if request.method == "POST":
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid:
-#             form.save()
-#             return HttpResponseRedirect(reverse("users:profile"))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     context = {
-#         "title": "Store - профиль",
-#         "form": form,
-#         "baskets": Basket.objects.filter(user=request.user),
-#     }
-#     return render(request, "users/profile.html", context)
-
-
-# def logout(request):
-#     auth.logout(request)
-#     return HttpResponseRedirect(reverse('index'))
